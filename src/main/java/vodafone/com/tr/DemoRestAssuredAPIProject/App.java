96ac6cd845ad251c0350f5bd884d5cf821fd579c
Sharma, Amrita, Vodafone Turkey (External) <Amrita.Sharma@vodafone.com>
2020-09-11 12:29:55 +0300

    Second comSmit

---
+++ src/main/java/vodafone/com/tr/DemoRestAssuredAPIProject/App.java
@@ -0,0 +1,13 @@
+package vodafone.com.tr.DemoRestAssuredAPIProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
